f907f771d418b008a344458d3948968fab412055
KLAIIIII <guyyg03546@gmail.com>
2022-08-29 16:56:36 +0700

    update

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ db.once("open",()=>{
     console.log("Connect to the databases")
 })
 
+// middleware
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(session({secret:"my secret key",saveUninitialized:true, resave:false }))
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+})
+
 app.use(morgan('dev'))
+
 // http://localhost${PORT}
 app.get('/',(req,res)=>{
     // return res.send("Home page")
