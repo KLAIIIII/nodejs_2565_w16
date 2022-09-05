4fceb40d44ffb33c0952f0ae304d1e6f7b38b9d8
KLAIIIII <guyyg03546@gmail.com>
2022-09-05 15:35:23 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -6,10 +6,15 @@ router.get('/', (req, res) => {
     return res.render("index", { title: "Home Page" })
 })
 
+router.get('/add', (req,res) =>{
+    res.render("add_users",{title:"Add Users"})
+})
+
 router.get('/users', (req, res) => {
     return res.status(200).json({
         message: "All Users"
     })
 })
+
 
 module.exports = router
