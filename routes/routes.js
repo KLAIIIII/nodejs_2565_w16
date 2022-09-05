c2615def04626ff4e9b99197bf2ad348e79f6d3c
KLAIIIII <guyyg03546@gmail.com>
2022-09-05 17:05:01 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -1,50 +1,62 @@
-const { render } = require('ejs')
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const User = require("../models/users")
 
-// Image upload
+//Image upload
 const storage = multer.diskStorage({
-    destination: function(req,res,cb){
-        cb(null,"./uploads")
+    destination: function (req, res, cb) {
+        cb(null, "./uploads")
     },
-    filename: function(req,file,cb){
-        cb(null,file.fieldname + "_" + Date.now() + "_" + file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
     }
 })
 
 const upload = multer({
-    storage:storage,
+    storage: storage,
 }).single("image")
 
-// Inser user into database rou
-router.post('/add',upload,(req,res)=>{
-    const user = new User ({
+//Insert user into database route
+router.post('/add', upload, (req, res) => {
+    const user = new User({
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone,
-        image: req.body.image
+        image: req.file.filename,
     })
-    user.save((err)=>{
-        if(err){
-            req.json({message:err.message,type:"warning"})
-        }else{
-            req.session.message={
-                type:"success",
-                message:"User added successfully!"
+    user.save((err) => {
+        if (err) {
+            req.json({
+                message: err.message, type: "danger"
+            })
+        } else {
+            req.session.message = {
+                type: "success",
+                message: "User added successfully!"
             }
             res.redirect("/")
         }
     })
 })
 
+//Get all users route
 router.get('/', (req, res) => {
-    return res.render("index", { title: "Home Page" })
+    // return res.render("index", { title: "Home Page" })
+    User.find().exec((err, users) => {
+        if (err) {
+            res.json({ message: err.message })
+        } else {
+            res.render("index", { 
+                title: "Home Page", 
+                users: users 
+            })
+        }
+    })
 })
 
-router.get('/add', (req,res) =>{
-    res.render("add_users",{title:"Add Users"})
+router.get("/add", (req, res) => {
+    res.render("add_users", { title: "Add Users" })
 })
 
 router.get('/users', (req, res) => {
@@ -52,6 +64,5 @@ router.get('/users', (req, res) => {
         message: "All Users"
     })
 })
-
 
 module.exports = router
